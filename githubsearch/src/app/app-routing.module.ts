239f469a2d717feef79c1aed1fabb1db6617612d
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-16 18:10:25 +0300

    routing the different components

--- githubsearch/src/app/app-routing.module.ts
+++ githubsearch/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SearchComponent} from "./search/search.component";
+import {HomeComponent} from "./home/home.component";
+import {RepositoriesComponent} from "./repositories/repositories.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'repositories', component: RepositoriesComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
