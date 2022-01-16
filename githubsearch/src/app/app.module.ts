cd2c87328d2397327dea124d880c936e90f8787c
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-16 17:35:26 +0300

    create navbar,home and search components,hide API link in git ignore files

--- githubsearch/src/app/app.module.ts
+++ githubsearch/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
