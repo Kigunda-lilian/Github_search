ce4794979ea7208e4fc449f14a4840498b0cd37d
lilian-2021 <lilian.kigunda@student.moringaschool.com>
2022-01-17 18:25:05 +0300

    added files, class to hold variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiKey : "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",
-  apiUrl: "https://api.github.com/users",
+  production: false
 };
 
 /*
